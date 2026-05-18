a6b808215abbd797a9aa5ea23cc68db0996fe961
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-18 09:34:54 -0300

    feat: aula2 - adiciona metodos com parametros e retorno

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -1,11 +1,58 @@
 package br.com.fiapride.main;
 
+<<<<<<< HEAD
+=======
 // Importamos a classe Passageiro para que o sistema a reconheça
+>>>>>>> dfa228c83fc4a3acd4d8a14c052a5355ce32cc3c
 import br.com.fiapride.model.Gato;
 
 public class SistemaPrincipal {
 
     public static void main(String[] args) {
+<<<<<<< HEAD
+
+        Gato gato1 = new Gato();
+        gato1.cor = "Laranja";
+        gato1.tamanho = 20; // cm
+        gato1.peso = 5; // kg
+
+        Gato gato2 = new Gato();
+        gato2.cor = "Cinza";
+        gato2.tamanho = 100; // cm
+        gato2.peso = 6; // kg
+
+        // Exibindo os dados iniciais
+        System.out.println("--- Sistema FiapRide ---");
+
+        System.out.println("\n ---Meu Gato--- ");
+        System.out.println("Cor: " + gato1.cor);
+        System.out.println("Tamanho inicial: " + gato1.tamanho + " cm");
+        System.out.println("Peso inicial: " + gato1.peso + " kg");
+
+        System.out.println("\n ---Gato do Meu Amigo--- ");
+        System.out.println("Cor: " + gato2.cor);
+        System.out.println("Tamanho inicial: " + gato2.tamanho + " cm");
+        System.out.println("Peso inicial: " + gato2.peso + " kg");
+
+        // evulucao gato 1
+        System.out.println("\n ---Evolução do Meu Gato--- ");
+
+        gato1.comer(1.5);
+        gato1.crescer(3);
+
+        System.out.println("Peso atual: " + gato1.peso + " kg");
+        System.out.println("Tamanho atual: " + gato1.tamanho + " cm");
+
+        // evolucao gato 2
+        System.out.println("\n ---Evolução do Gato do Meu Amigo--- ");
+
+        gato2.comer(-1);
+        gato2.crescer(3);
+
+        System.out.println("Peso atual: " + gato2.peso + " kg");
+        System.out.println("Tamanho atual: " + gato2.tamanho + " cm");
+
+=======
         // INSTANCIAÇÃO
         // O comando 'new' aloca memória para um novo objeto.
         // Criando o primeiro passageiro (Objeto 1)
@@ -26,5 +73,6 @@ public class SistemaPrincipal {
         System.out.println("O gato do meu amigo é: " + gato2.cor);
         
         // Teste mental: Se eu mudar o nome do passageiro1, o passageiro2 muda?
+>>>>>>> dfa228c83fc4a3acd4d8a14c052a5355ce32cc3c
     }
 }
--- src/br/com/fiapride/model/Gato.java
+++ src/br/com/fiapride/model/Gato.java
@@ -1,8 +1,42 @@
 package br.com.fiapride.model;
+<<<<<<< HEAD
+
+public class Gato {
+
+    public String cor;
+    public double tamanho;
+    public double peso;
+
+
+    // Método para o gato comer
+    public void comer(double quantidadeComida) {
+
+        // Regra de negócio
+        if (quantidadeComida <= 0) {
+            System.out.println("Quantidade de comida negativa! (alimenta esse gato)");
+        } else {
+            peso += quantidadeComida;
+            System.out.println("O gato comeu e agora pesa: " + peso + " kg");
+        }
+    }
+
+    // Método para o gato crescer
+    public void crescer(double crescimento) {
+
+        // Regra de negócio
+        if (crescimento <= 0) {
+            System.out.println("O gato não teve crescimento");
+        } else {
+            tamanho += crescimento;
+            System.out.println("O gato cresceu e agora mede: " + tamanho + " cm");
+        }
+    }
+=======
 public class Gato { // <-- Use o nome do SEU objeto    
     // As características que você pensou    
     public String cor;
     public float peso;
     public float tamanho;
     
+>>>>>>> dfa228c83fc4a3acd4d8a14c052a5355ce32cc3c
 }
